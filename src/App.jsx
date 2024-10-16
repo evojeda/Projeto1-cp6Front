0d789901b6028193c50c872f0eaf3d504cd8f169
Evellyn Valencia <evellyn_2701@hotmail.com>
2024-10-16 11:58:27 -0300

    Criando a estrutura do site

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from './components/Nav/Nav.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Nav/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <h2>© Copyright 2021 - GourmetOn - Todos os direitos reservados GourmetOn com Agência de Restaurantes Online S.A.
+        </h2>
+        <p>Termos e condições de uso - Privacidade</p>
+      </footer>
+    );
+  }
+  
+  export default Footer
